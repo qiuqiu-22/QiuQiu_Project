5419089b90ea5fb4a5cf210ff3bb420c9ecc1175
景秋 罗 <2286477428@qq.com>
2023-09-30 14:54:27 +0800

    修改test1为求解最大公约数

--- QiuQiu_Project/test1.cpp
+++ QiuQiu_Project/test1.cpp
@@ -1,6 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
 {
-	printf("abc \ n");
+	int a, b, c;
+	printf("请输入两个数 \n");
+	scanf("%d%d", &a, &b);
+	c = a % b;
+	while (c != 0)
+	{
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	printf("最大公约数为 %d \n", b);
 	return(0);
 }
